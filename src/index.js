648b7d71c382c424f336a49ecaa80a93be3df0f9
js0205 <467353069@qq.com>
2022-10-14 21:26:07 +0800

    feat: image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import { getBlogPosts } from "./data";
-console.log(getBlogPosts());
+import "./style.css";
+import HeroImage from "./assets/images/hero.jpeg";
+const blogs = getBlogPosts();
+const ul = document.createElement("ul");
+blogs.forEach((blog) => {
+    const li = document.createElement("li");
+    li.innerText = blog;
+    ul.appendChild(li);
+});
+document.body.appendChild(ul);
+const image = document.createElement("img");
+image.src = HeroImage;
+document.body.prepend(image);
+// const h1 = document.createElement("h1");
+// h1.innerText = "博客列表";
+// document.body.prepend(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
     mode: "development",
+    devtool: "inline-source-map",
     entry: "./src/index.js",
     output: {
-        filename: "dist.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "博客列表",
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
     },
 };
